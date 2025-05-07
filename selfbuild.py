e7f6dc11a59f9e5a7c5345bbefb7c2afd553dbfd
Gabin-A <gabin.angeloz@student.unisg.ch>
2025-05-07 19:20:49 +0200

    change in streamlitapp

--- selfbuild.py
+++ selfbuild.py
@@ -2,4 +2,5 @@ import streamlit as st
 st.write('why doesnt this work')
 st.write('now its running')
 st.write('dman it does autosync')
-st write('autosync or what?')
+st.write('autosync or what?')
+st.write('test with the back open')
